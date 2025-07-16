b55e7979b78db09ac1b50db4cefeed70861dfe33
langdutybyte <ltl0009107417@gmail.com>
2025-07-16 10:05:02 +0800

    lodash1

--- tianlang_2000-lodash.js
+++ tianlang_2000-lodash.js
@@ -1,3 +1,11 @@
-var compact = function() {
-  
+var tianlang_2000 = function() {
+  function compact(Array) {
+    var result = []
+    for (var i = 0; i < Array.length; i++) {
+      if (Array[i] !== null || undefined || 0 || NaN || '') {
+        result.push(Array[i])
+      }
+    }
+    return result
+  }
 }
