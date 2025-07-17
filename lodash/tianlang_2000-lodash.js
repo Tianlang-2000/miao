5b9f70999b31d50e04c360271691184ff42c4a16
langdutybyte <ltl0009107417@gmail.com>
2025-07-17 11:17:51 +0800

    lodash3

--- lodash/tianlang_2000-lodash.js
+++ lodash/tianlang_2000-lodash.js
@@ -7,5 +7,25 @@ var tianlang_2000 = {
       }
     }
     return result
+  },
+
+  chunk: function (array, size) {
+    if (!Array.isArray(array) || size <= 0) return []
+    const result = []
+    for (var i = 0; i < array.length; i += size) {
+      result.push(array.slice(i, i + size))
+    }
+    return result
+  },
+
+  fill: function (array, value, start = 0, end = array.length) {
+    const result = []
+    for (var i = 0; i < array.length; i++) {
+      if (start <= i < end) {
+        result.push(value)
+      }
+      result.push(array[i])
+    }
+    return result
   }
 }
