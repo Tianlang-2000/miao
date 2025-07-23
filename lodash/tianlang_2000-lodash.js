71514f3591f06f1b688116301935561c3bf1e65b
langdutybyte <ltl0009107417@gmail.com>
2025-07-23 11:16:50 +0800

    lodash4

--- lodash/tianlang_2000-lodash.js
+++ lodash/tianlang_2000-lodash.js
@@ -21,11 +21,122 @@ var tianlang_2000 = {
   fill: function (array, value, start = 0, end = array.length) {
     const result = []
     for (var i = 0; i < array.length; i++) {
-      if (start <= i < end) {
+      if (start <= i && i < end) {
         result.push(value)
+      }else {
+        result.push(array[i])
       }
-      result.push(array[i])
     }
     return result
+  },
+
+  drop: function (array, n = 1) {
+    if (n >= array.length) {
+      return []
+    } else {
+      return array.slice(n)
+    }
+  },
+
+  findindex: function (array, predicate, fromIndex = 0) {
+    if (typeof predicate === 'function') {
+      for (let i = fromIndex; i < array.length; i++) {
+        if (predicate(array[i])) {
+          return i
+        }
+      }
+    } else if (typeof predicate === 'object') {
+      for (let i = fromIndex; i < array.length; i++) {
+        let match = true;
+        for (let key in predicate) {
+          if (array[i][key] !== predicate[key]) {
+            match = false
+            break
+          }
+        }
+        if (match) return i
+      }
+    } else if (typeof predicate === 'string') {
+      for (let i = fromIndex; i < array.length; i++) {
+        if (array[i][predicate]) {
+          return i
+        }
+      }
+    } else if (Array.isArray(predicate)) {
+      let [key, value] = predicate
+      for (let i = 0; i < array.length; i++) {
+        if (array[i][key] == value) {
+          return i
+        }
+      }
+    }
+  },
+
+  findindex: function (array, predicate, fromIndex = 0) {
+    if (typeof predicate === 'function') {
+      for (let i = array.length - 1; i >= fromIndex; i--) {
+        if (predicate(array[i])) {
+          return i
+        }
+      }
+    } else if (typeof predicate === 'object') {
+      for (let i = array.length - 1; i >= fromIndex; i--) {
+        let match = true;
+        for (let key in predicate) {
+          if (array[i][key] !== predicate[key]) {
+            match = false
+            break
+          }
+        }
+        if (match) return i
+      }
+    } else if (typeof predicate === 'string') {
+      for (let i = array.length - 1; i >= fromIndex; i--) {
+        if (array[i][predicate]) {
+          return i
+        }
+      }
+    } else if (Array.isArray(predicate)) {
+      let [key, value] = predicate
+      for (let i = array.length - 1; i >= fromIndex; i--) {
+        if (array[i][key] == value) {
+          return i
+        }
+      }
+    }
+  },
+
+  flatten: function (array) {
+    let result = []
+    for (let i = 0; i < array.length; i++) {
+      if (Array.isArray(array[i])) {
+        for (let j = 0; j < array[i].length; j++) {
+          result.push(array[i][j])
+        }
+      } else {
+        result.push(array[i])
+      }
+    }
+    return result
+  },
+
+  flattendeep: function (array) {
+    let result = []
+
+    flattendeep1 = function(arr) {
+      for (let i = 0; i < arr.length; i++) {
+        if (Array.isArray(arr[i])) {
+          flattendeep1(arr[i])
+        } else {
+        result.push(arr[i])
+        }
+      }
+    }
+    flattendeep1(array)
+    return result
+  },
+
+  flattendepth: function (array, depth = 1) {
+
   }
 }
